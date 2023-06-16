ab1581cf242b39817747d08aa980a1b225f176ce
bielmorais <bielmorais8000@gmail.com>
2023-06-15 22:39:19 -0300

    parametros
    aprendi a criar e utilizar parametros,

--- Express/app.js
+++ Express/app.js
@@ -14,6 +14,12 @@ app.get("/blog", function(req, res){
     res.sendFile(__dirname+"/index.html") 
 });
 
+app.get("/ola/:cargo/:nome", function(req, res){
+    res.send("<h1>Ola "+ req.params.nome +"</h1><br><h2>Seu cargo: "+ req.params.cargo + "</h2>");
+    // o .send só pode ser utilizado uma ves dentro de um parametro
+
+})
+
 app.listen(8081, function(){
     console.log("Servidor rodando na url: http://localhost:8081")
 }); //tem que ser a ultima linha do código
