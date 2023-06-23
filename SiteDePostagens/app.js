e0d75d3d360a047c6ad892b06d2d8e8aaf5dde4f
bielmorais <bielmorais8000@gmail.com>
2023-06-22 22:16:36 -0300

    body-parser
    utilizando o body-parser e organizando separadamente as funções em arquivos específicos

--- SiteDePostagens/app.js
+++ SiteDePostagens/app.js
@@ -1,23 +1,29 @@
 const express = require("express");
 const app = express();
-const Sequelize = require('sequelize');
 const handlebars = require('express-handlebars');
+const bodyParser = require('body-parser')
+const db= require('./models/db') 
 
 //Config
     //Template Engine
         app.engine('handlebars', handlebars.engine({defaultLayout: 'main'}));
         app.set('view engine', 'handlebars');
 
-    // Conexão com o banco de dados MySql    
-        const sequelize = new Sequelize("SistemaDeCadastro", "root", "",{
-            host: "localhost",
-            dialect: "mysql"
-        });
+    // Body Parser 
+
+    app.use(bodyParser.urlencoded({extended: false}))
+    app.use(bodyParser.json())
+
 
 // Rotas
 
         app.get('/cad', function(req, res){
             res.render("formulario")
+        })
+
+        app.post('/add', function(req, res){
+            req.body.conteudo
+            res.send("FORMULÁRIO RECEBIDO!! <br><br> Titulo: "+req.body.titulo+"<br>Conteúdo: "+ req.body.conteudo)
         })
 
 
