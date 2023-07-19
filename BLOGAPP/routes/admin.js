78e76ad9a3ac698661d925d242b01c9b17d79fa2
bielmorais <bielmorais8000@gmail.com>
2023-07-18 23:45:41 -0300

    Editar categoria [Erro presente]
    erro ao editar a categoria

--- BLOGAPP/routes/admin.js
+++ BLOGAPP/routes/admin.js
@@ -13,7 +13,7 @@ router.get('/posts', (req, res) => {
 });
 
 router.get('/categorias', (req, res) => {
-    Categoria.find().lean().then((categorias) => {
+    Categoria.find().lean().then((categorias) => { // .lean() optimiza o retorno pra js
         res.render("admin/categorias", {categorias: categorias});
     }).catch((error) => {
         req.flash("error_msg", "Houve um erro ao listar as categorias!");
@@ -40,6 +40,9 @@ router.post('/categorias/nova', (req, res) => {
     if(req.body.nome.length < 2){
         erros.push({texto: "Nome da categoria muito curto"});
     };
+    if(req.body.slug.length < 2){
+        erros.push({texto: "Slug da categoria muito curto"});
+    };
     
     if(erros.length > 0){
         res.render('admin/addcategorias', {erros: erros})
@@ -57,6 +60,55 @@ router.post('/categorias/nova', (req, res) => {
             console.log("Erro ao salvar categoria"+ error);
         })
     }
-})
+});
 
+router.get('/categorias/edit/:id', (req, res) => {
+    Categoria.findOne({_id:req.params.id}).lean().then((categoria) => {
+        res.render('admin/editcategorias', {categoria: categoria})
+    }).catch((error) => {
+        req.flash('error_msg', "Esta categoria não existe");
+        res.redirect('/admin/categorias');
+    })
+    
+});
+
+router.post('/categorias/edit', (req, res) =>{
+    Categoria.findOne({_id: req.body.id}).then((categoria) => {
+        // var erros = [];
+
+        // if(!req.body.nome || typeof req.body.nome == undefined || req.body.nome == null ){
+        //     erros.push({texto: "Nome inválido"});
+        // };
+        // if(!req.body.slug || typeof req.body.slug == undefined || req.body.slug == null){
+        //     erros.push({texto: "Slug inváligo"});
+        // };
+
+        // if(req.body.nome.length < 2){
+        //     erros.push({texto: "Nome da categoria muito curto"});
+        // };
+        // if(req.body.slug.length < 2){
+        //     erros.push({texto: "Slug da categoria muito curto"});
+        // };
+
+        // if(erros.length > 0){
+            // res.render('admin/editcategorias/', {erros: erros});
+        // }else{
+            Categoria.nome = req.body.nome;
+            Categoria.slug = req.body.slug;
+
+            
+            Categoria.save().then(() => {
+                req.flash('success_msg', "Categoria editada com sucesso!");
+                res.redirect('/admin/categorias');
+            }).catch((error) => {
+                req.flash('error_msg', "Houve um erro ao editar a categoria!");
+                res.redirect('/admin/categorias');
+            })
+        // }
+            
+    }).catch((error) => {
+        req.flash('error_msg', "Houve um erro ao editar a categoria");
+        res.redirect('/admin/categorias');
+    })
+})
 module.exports = router
